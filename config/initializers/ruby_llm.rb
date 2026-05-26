ccc32a98362f43de5745d2e57917f1e1a00c0dd4
thierry praud <praud.thierry@wanadoo.fr>
2026-05-26 16:08:07 +0200

    chatcreate index show gem installée

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,5 +1,5 @@
 RubyLLM.configure do |config|
   # config.openai_api_key = ENV["OPENAI_API_KEY"]
-  config.openai_api_key = ENV["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"]
+  config.openai_api_key = ENV["OPENAI_API_KEY"]
   config.openai_api_base = "https://models.inference.ai.azure.com"
 end
