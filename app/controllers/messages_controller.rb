f0abfc0ebf54844261cb77acb46f2d9f023019a2
thierry praud <praud.thierry@wanadoo.fr>
2026-05-27 15:36:42 +0200

    a

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
-class MessagesController < ApplicationController
-  before_action :authenticate_user!
+# class MessagesController < ApplicationController
+  # before_action :authenticate_user!
   #   def create
   #     SYSTEM_PROMPT = "You are a Teaching Assistant.\n\nI am a student at the Le Wagon AI Software Development Bootcamp, learning how to code.\n\nHelp me break down my problem into small, actionable steps, without giving away solutions.\n\nAnswer concisely in Markdown."
 
