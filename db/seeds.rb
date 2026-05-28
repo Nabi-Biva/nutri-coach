5eb1b368367b64f31bed80b8022c0e85aa8c1272
Mahfoudhi Ghzela <ghzelamahfoudhi@yahoo.fr>
2026-05-28 11:12:49 +0200

    my commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,6 @@ User.destroy_all
 # -------------------------------------------------------------------
 # 2. USERS
 # -------------------------------------------------------------------
-User.destroy_all
-Profile.destroy_all
-Recipe.destroy_all
-Chat.destroy_all
-Message.destry_all
 
 emma = User.create!(
   email:                 "emma@example.com",
